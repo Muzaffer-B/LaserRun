e4a8c2687a7c83307b67121ba383def843c355a1
MuzafferB <muzo626@hotmail.com>
2021-07-26 22:10:07 +0300

    LaserRunner v0.1

---
+++ LaserRun/Assets/Codes/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+
+    public float speedH = 2f;
+    public float speedV = 2f;
+    public Vector3 distance;
+
+  
+
+    void Start()
+    {
+        distance = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + distance;
+
+    }
+}
---
+++ LaserRun/Assets/Codes/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Player;
+    public float speed;
+    public float jump;
+
+    Rigidbody physic;
+    Animator anim;
+
+    void Start()
+    {
+        physic = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Player.transform.Translate(0, 0, Time.deltaTime * speed);
+            //anim.SetTrigger("Run");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            Player.transform.Translate(Time.deltaTime * -speed, 0, 0);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            Player.transform.Translate(Time.deltaTime * speed, 0, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+                Player.transform.Translate(0, Time.deltaTime * jump, 0);
+                anim.SetTrigger("Jump");
+
+
+        }
+    }
+}
